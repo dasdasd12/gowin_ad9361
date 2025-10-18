2e5ace9820f2213169cc484a5dc27ec8247ec5db
wxhenry <wyh41021@sina.com>
2025-10-18 15:34:22 +0800

    pause signal

--- async_fifo/async_fifo.v
+++ async_fifo/async_fifo.v
@@ -1,97 +1,113 @@
 // distributed under the mit license
 // https://opensource.org/licenses/mit-license.php
 
-`timescale 1 ns / 1 ps
-`default_nettype none
+`timescale 1 ns / 1 ps `default_nettype none
 
-module async_fifo
-
-    #(
-        parameter DSIZE = 8,
-        parameter ASIZE = 4,
-        parameter FALLTHROUGH = "TRUE" // First word fall-through without latency
-    )(
-        input  wire             wclk,
-        input  wire             wrst_n,
-        input  wire             winc,
-        input  wire [DSIZE-1:0] wdata,
-        output wire             wfull,
-        output wire             awfull,
-        input  wire             rclk,
-        input  wire             rrst_n,
-        input  wire             rinc,
-        output wire [DSIZE-1:0] rdata,
-        output wire             rempty,
-        output wire             arempty
-    );
+module async_fifo #(
+    parameter DSIZE        = 8,
+    parameter ASIZE        = 4,
+    parameter FALLTHROUGH  = "TRUE",                 // First word fall-through without latency
+    parameter PAUSE_THRESH = (1 << (ASIZE + 1)) / 3  // 默认2/3的pause帧门限
+) (
+    input  wire             wclk,
+    input  wire             wrst_n,
+    input  wire             winc,
+    input  wire [DSIZE-1:0] wdata,
+    output wire             wfull,
+    output wire             awfull,
+    input  wire             rclk,
+    input  wire             rrst_n,
+    input  wire             rinc,
+    output wire [DSIZE-1:0] rdata,
+    output wire             rempty,
+    output wire             arempty,
+    output wire             need_pause
+);
 
     wire [ASIZE-1:0] waddr, raddr;
-    wire [ASIZE  :0] wptr, rptr, wq2_rptr, rq2_wptr;
+    wire [ASIZE : 0] wptr, rptr, wq2_rptr, rq2_wptr;
 
     // The module synchronizing the read point
     // from read to write domain
-    sync_r2w
-    #(ASIZE)
-    sync_r2w (
-    .wq2_rptr (wq2_rptr),
-    .rptr     (rptr),
-    .wclk     (wclk),
-    .wrst_n   (wrst_n)
+    sync_r2w #(ASIZE) sync_r2w (
+        .wq2_rptr(wq2_rptr),
+        .rptr    (rptr),
+        .wclk    (wclk),
+        .wrst_n  (wrst_n)
     );
 
     // The module synchronizing the write point
     // from write to read domain
-    sync_w2r
-    #(ASIZE)
-    sync_w2r (
-    .rq2_wptr (rq2_wptr),
-    .wptr     (wptr),
-    .rclk     (rclk),
-    .rrst_n   (rrst_n)
+    sync_w2r #(ASIZE) sync_w2r (
+        .rq2_wptr(rq2_wptr),
+        .wptr    (wptr),
+        .rclk    (rclk),
+        .rrst_n  (rrst_n)
     );
 
     // The module handling the write requests
-    wptr_full
-    #(ASIZE)
-    wptr_full (
-    .awfull   (awfull),
-    .wfull    (wfull),
-    .waddr    (waddr),
-    .wptr     (wptr),
-    .wq2_rptr (wq2_rptr),
-    .winc     (winc),
-    .wclk     (wclk),
-    .wrst_n   (wrst_n)
+    wptr_full #(ASIZE) wptr_full (
+        .awfull  (awfull),
+        .wfull   (wfull),
+        .waddr   (waddr),
+        .wptr    (wptr),
+        .wq2_rptr(wq2_rptr),
+        .winc    (winc),
+        .wclk    (wclk),
+        .wrst_n  (wrst_n)
     );
 
     // The DC-RAM
-    fifomem
-    #(DSIZE, ASIZE, FALLTHROUGH)
-    fifomem (
-    .rclken (rinc),
-    .rclk   (rclk),
-    .rdata  (rdata),
-    .wdata  (wdata),
-    .waddr  (waddr),
-    .raddr  (raddr),
-    .wclken (winc),
-    .wfull  (wfull),
-    .wclk   (wclk)
+    fifomem #(DSIZE, ASIZE, FALLTHROUGH) fifomem (
+        .rclken(rinc),
+        .rclk  (rclk),
+        .rdata (rdata),
+        .wdata (wdata),
+        .waddr (waddr),
+        .raddr (raddr),
+        .wclken(winc),
+        .wfull (wfull),
+        .wclk  (wclk)
     );
 
     // The module handling read requests
-    rptr_empty
-    #(ASIZE)
-    rptr_empty (
-    .arempty  (arempty),
-    .rempty   (rempty),
-    .raddr    (raddr),
-    .rptr     (rptr),
-    .rq2_wptr (rq2_wptr),
-    .rinc     (rinc),
-    .rclk     (rclk),
-    .rrst_n   (rrst_n)
+    rptr_empty #(ASIZE) rptr_empty (
+        .arempty (arempty),
+        .rempty  (rempty),
+        .raddr   (raddr),
+        .rptr    (rptr),
+        .rq2_wptr(rq2_wptr),
+        .rinc    (rinc),
+        .rclk    (rclk),
+        .rrst_n  (rrst_n)
     );
+
+    // 使用wptr与wq2_rptr，在写时钟域计算需要暂停信号
+    wire [ASIZE : 0] wptr_bin, rptr_bin;
+    gray2bin #(
+        .DATA_W(ASIZE + 1)
+    ) gray2bin_inst (
+        .clk  (wclk),
+        .rst_n(wrst_n),
+        .gray (wptr),
+        .bin  (wptr_bin)
+    );
+
+    gray2bin #(
+        .DATA_W(ASIZE + 1)
+    ) gray2bin_inst2 (
+        .clk  (wclk),
+        .rst_n(wrst_n),
+        .gray (wq2_rptr),
+        .bin  (rptr_bin)
+    );
+
+    // 计算FIFO中数据的数量
+    wire [ASIZE:0] diff;
+    assign diff       = wptr_bin - rptr_bin;
+
+    // 计算need_pause信号
+    assign need_pause = (diff >= PAUSE_THRESH);
 
 endmodule
 
---
+++ async_fifo/async_fifo_tb.v
@@ -0,0 +1,85 @@
+`timescale 1ns / 100ps
+//---------------------------------------------------------------------------------------- 
+// Created by:             wxhenry
+// Created date:           2025/10/18 15:01:35 
+// Version:                V1.0 
+// File Name:              async_fifo_tb.v 
+// Descriptions:            
+//                          
+//---------------------------------------------------------------------------------------- 
+
+`define FINISH_TIME 1000
+`define CYCLE 1
+`define CYCLE_HALF 0.5
+`define WAIT_CYCLE(n) repeat(n) @(posedge clk);
+
+module async_fifo_tb ();
+    reg clk, rclk;
+    reg rst_n;
+
+    initial begin
+        rclk = 1'b1;
+        forever #`CYCLE rclk = ~rclk;
+    end
+    initial begin
+        clk = 1'b1;
+        forever #`CYCLE_HALF clk = ~clk;
+    end
+    initial begin
+        rst_n = 1'b0;
+        #2.5 rst_n = 1'b1;
+    end
+    initial begin
+        $dumpfile("async_fifo_tb_wave.vcd");
+        $dumpvars(0, async_fifo_tb);
+        #`FINISH_TIME $finish;
+    end
+
+    // output declaration of module async_fifo
+    parameter DSIZE = 8;
+    wire             wfull;
+    wire             awfull;
+    wire [DSIZE-1:0] rdata;
+    wire             rempty;
+    wire             arempty;
+    wire             need_pause;
+
+    reg              winc;
+    initial begin
+        winc = 1'b1;
+        #295 winc = 1'b0;
+    end
+
+    async_fifo #(
+        .DSIZE      (8),
+        .ASIZE      (6),
+        .FALLTHROUGH("FALSE")
+    ) u_async_fifo (
+        .wclk      (clk),
+        .wrst_n    (rst_n),
+        .winc      (winc),
+        .wdata     (12),
+        .wfull     (wfull),
+        .awfull    (awfull),
+        .rclk      (rclk),
+        .rrst_n    (rst_n),
+        .rinc      (1),
+        .rdata     (rdata),
+        .rempty    (rempty),
+        .arempty   (arempty),
+        .need_pause(need_pause)
+    );
+
+    wire pause;
+
+    gen_pause #(
+        .CD_CYCLE(10)
+    ) u_gen_pause (
+        .clk       (clk),
+        .rst_n     (rst_n),
+        .need_pause(need_pause),
+        .pause     (pause)
+    );
+
+
+endmodule
